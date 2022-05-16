cbaedd6d6e5fde14608e76fcc1dd33e3c62497c0
儿时 <58261676+childrentime@users.noreply.github.com>
2022-05-16 17:44:51 +0800

    🧪 test: add parse test

--- src/__test/parser.test.ts
+++ src/__test/parser.test.ts
@@ -151,4 +151,316 @@ describe("parse", () => {
       })
     );
   });
+
+  it("should parse var declaration", () => {
+    const input = "var a = 1;";
+    const ast = parse(input);
+    expect(JSON.stringify(ast)).toStrictEqual(
+      JSON.stringify({
+        type: "Program",
+        body: [
+          {
+            type: "VariableDeclaration",
+            declarations: [
+              {
+                type: "VariableDeclarator",
+                id: {
+                  type: "Identifier",
+                  name: "a",
+                },
+                init: {
+                  type: "Literal",
+                  value: "1",
+                },
+              },
+            ],
+            kind: "var",
+          },
+        ],
+        sourceType: "script",
+      })
+    );
+  });
+
+  it("should parse for statement", () => {
+    const input = `for(var a =1; a<3;a++){
+      var b = 1;
+    }`;
+    const ast = parse(input);
+    expect(JSON.stringify(ast)).toStrictEqual(
+      JSON.stringify({
+        type: "Program",
+        body: [
+          {
+            type: "ForStatement",
+            init: {
+              type: "VariableDeclaration",
+              declarations: [
+                {
+                  type: "VariableDeclarator",
+                  id: {
+                    type: "Identifier",
+                    name: "a",
+                  },
+                  init: {
+                    type: "Literal",
+                    value: "1",
+                  },
+                },
+              ],
+              kind: "var",
+            },
+            test: {
+              type: "BinaryExpression",
+              operator: "<",
+              left: {
+                type: "Identifier",
+                name: "a",
+              },
+              right: {
+                type: "Literal",
+                value: "3",
+              },
+            },
+            update: {
+              type: "UpdateExpression",
+              operator: "++",
+              argument: {
+                type: "Identifier",
+                name: "a",
+              },
+              prefix: false,
+            },
+            body: {
+              type: "BlockStatement",
+              body: [
+                {
+                  type: "VariableDeclaration",
+                  declarations: [
+                    {
+                      type: "VariableDeclarator",
+                      id: {
+                        type: "Identifier",
+                        name: "b",
+                      },
+                      init: {
+                        type: "Literal",
+                        value: "1",
+                      },
+                    },
+                  ],
+                  kind: "var",
+                },
+              ],
+            },
+          },
+        ],
+        sourceType: "script",
+      })
+    );
+  });
+
+  it("should parse if statement", () => {
+    const input = `if(a === 1){
+      a = 2;
+    }else {
+      a = 3;
+    }`;
+    const ast = parse(input);
+    expect(JSON.stringify(ast)).toStrictEqual(
+      JSON.stringify({
+        type: "Program",
+        body: [
+          {
+            type: "IfStatement",
+            test: {
+              type: "BinaryExpression",
+              operator: "===",
+              left: {
+                type: "Identifier",
+                name: "a",
+              },
+              right: {
+                type: "Literal",
+                value: "1",
+              },
+            },
+            consequent: {
+              type: "BlockStatement",
+              body: [
+                {
+                  type: "ExpressionStatement",
+                  expression: {
+                    type: "AssignmentExpression",
+                    operator: "=",
+                    left: {
+                      type: "Identifier",
+                      name: "a",
+                    },
+                    right: {
+                      type: "Literal",
+                      value: "2",
+                    },
+                  },
+                },
+              ],
+            },
+            alternate: {
+              type: "BlockStatement",
+              body: [
+                {
+                  type: "ExpressionStatement",
+                  expression: {
+                    type: "AssignmentExpression",
+                    operator: "=",
+                    left: {
+                      type: "Identifier",
+                      name: "a",
+                    },
+                    right: {
+                      type: "Literal",
+                      value: "3",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+        sourceType: "script",
+      })
+    );
+  });
+
+  it("should parse return statement", () => {
+    const input = `function a(){
+      return 1+2;
+    }`;
+    const ast = parse(input);
+    expect(JSON.stringify(ast)).toStrictEqual(
+      JSON.stringify({
+        type: "Program",
+        body: [
+          {
+            type: "FunctionDeclaration",
+            id: {
+              type: "Identifier",
+              name: "a",
+            },
+            params: [],
+            body: {
+              type: "BlockStatement",
+              body: [
+                {
+                  type: "ReturnStatement",
+                  argument: {
+                    type: "BinaryExpression",
+                    operator: "+",
+                    left: {
+                      type: "Literal",
+                      value: "1",
+                    },
+                    right: {
+                      type: "Literal",
+                      value: "2",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+        sourceType: "script",
+      })
+    );
+  });
+
+  it("should test while statement", () => {
+    const input = `while(a === 1){
+      var b = 2;
+    }`;
+    const ast = parse(input);
+    expect(JSON.stringify(ast)).toStrictEqual(
+      JSON.stringify({
+        type: "Program",
+        body: [
+          {
+            type: "WhileStatement",
+            test: {
+              type: "BinaryExpression",
+              operator: "===",
+              left: {
+                type: "Identifier",
+                name: "a",
+              },
+              right: {
+                type: "Literal",
+                value: "1",
+              },
+            },
+            body: {
+              type: "BlockStatement",
+              body: [
+                {
+                  type: "VariableDeclaration",
+                  declarations: [
+                    {
+                      type: "VariableDeclarator",
+                      id: {
+                        type: "Identifier",
+                        name: "b",
+                      },
+                      init: {
+                        type: "Literal",
+                        value: "2",
+                      },
+                    },
+                  ],
+                  kind: "var",
+                },
+              ],
+            },
+          },
+        ],
+        sourceType: "script",
+      })
+    );
+  });
+
+  it("should parse call expression", () => {
+    const input = "a(b,c,d)";
+    const ast = parse(input);
+    expect(JSON.stringify(ast)).toStrictEqual(
+      JSON.stringify({
+        type: "Program",
+        body: [
+          {
+            type: "ExpressionStatement",
+            expression: {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "a",
+              },
+              arguments: [
+                {
+                  type: "Identifier",
+                  name: "b",
+                },
+                {
+                  type: "Identifier",
+                  name: "c",
+                },
+                {
+                  type: "Identifier",
+                  name: "d",
+                },
+              ],
+            },
+          },
+        ],
+        sourceType: "script",
+      })
+    );
+  });
 });
