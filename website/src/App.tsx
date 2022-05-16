fc469a91b3fe6ba1c3a773ad33ef1e2f10f1745a
儿时 <58261676+childrentime@users.noreply.github.com>
2022-05-16 23:54:43 +0800

    ✨ feat: add headPage

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,5 +1,17 @@
+import Editor from "@monaco-editor/react";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="left">
+        <Editor
+          defaultLanguage="javascript"
+          defaultValue="console.log('hello world');"
+        />
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 }
 
 export default App;
